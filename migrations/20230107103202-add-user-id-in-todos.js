538fcb9a380da8891a9459ec89f397bcfc4ee028
vyshu-sathunuri <sathunurivaishanvi@gmail.com>
2023-01-07 16:06:03 +0530

    level10

--- migrations/20230107103202-add-user-id-in-todos.js
+++ migrations/20230107103202-add-user-id-in-todos.js
@@ -1,8 +1,14 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
     await queryInterface.addColumn("Todos", "userId", {
       type: Sequelize.DataTypes.INTEGER,
     });
@@ -15,20 +21,15 @@ module.exports = {
         field: "id",
       },
     });
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add reverting commands here.
      *
      * Example:
      * await queryInterface.dropTable('users');
      */
-  }
+    await queryInterface.removeColumn("Todos", "userId");
+  },
 };
